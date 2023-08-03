70648d94ba4a2d9fd72ca0bc4274f94294eb5faa
jclingan <jclingan@redhat.com>
2023-08-02 20:58:31 -0700

    Original revision

---
+++ deploy_mssql.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]]
+then
+  echo "usage: $0 [deploy | undeploy]"
+  exit 1
+elif [[ "$1" != "deploy" && "$1" != "undeploy" ]]
+then
+  echo "usage: $0 [deploy | undeploy]"
+  exit 1
+else
+  command=$1
+fi
+
+if [[ $command = "deploy" ]]
+then
+  oc new-project mssql
+#  oc adm policy add-scc-to-user anyuid system:serviceaccount:mssql:default
+  oc create secret generic mssql --from-literal=SA_PASSWORD="Sql2017fordemo"
+  oc apply -f ./storage.yaml
+  oc apply -f ./deployment.yaml
+elif [[ $command = "undeploy" ]]
+then
+  oc delete project mssql
+fi
+
